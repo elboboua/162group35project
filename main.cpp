8a50b7b5d57f76a35aacc508398dd1458a50fa59
Eben <wighte@oregonstate.edu>
2018-11-02 04:00:13 -0700

    menus

--- main.cpp
+++ main.cpp
@@ -1,21 +1,51 @@
 /*********************************************************************
 ** Author: Group 35
 ** Date: 11-4-2018
-** Description: The main file for the 162 Group Project 
+** Description: The main file for the 162 Group Project
 *********************************************************************/
 #include "Game.hpp"
-
+#include "Menu.hpp"
+#include "inputHandler.hpp"
 
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 
-int main () {
+// menu choice and prompt constants
+const std::string HowManyAnts = "How Many Ants would you like to spawn?";
+const std::string HowManBugs = "How Many Doodlebugs would you like to spawn?";
+const std::string antNo = "Ant No. ";
+const std::string bugNo = "Doodlebug No. ";
+const std::string startingX = "'s starting X position";
+const std::string startingY = "'s starting Y position";
+int main() {
 
-	srand(time(NULL));
+    Game game;
+    Menu menu;
 
-	Game game;
-	game.loop();	
+    int numAnts = 1;
+    int nnumBugs = 1;
+    menu.singleInteger(HowManyAnts, numAnts, false);
 
-	return 0;
+    for (int a = 0; a < numAnts; a++) {
+
+        int startX = 3;
+        menu.singleInteger(antNo + std::to_string(a) + startingX, startX, false);
+
+        int startY = 3;
+        menu.singleInteger(antNo + std::to_string(a) + startingY, startY, false);
+    }
+
+    // const std::string bugsStartingX = "Doodlebug's starting X position";
+    // int bugX = 3;
+    // menu.singleInteger(bugsStartingX, bugX, false);
+    //
+    // const std::string bugsStartingY = "Doodlebug's starting Y position";
+    // int bugY = 3;
+    // menu.singleInteger(bugsStartingY, bugY, false);
+    srand(time(NULL));
+
+    game.loop();
+
+    return 0;
 }
