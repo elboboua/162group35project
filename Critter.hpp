e97b47deb148b4509301f6b0e87e28853fa24cd1
Eben <wighte@oregonstate.edu>
2018-11-01 22:51:33 -0700

    add check surroundings function to make breeding, moving, and eating functions easier to implement

--- Critter.hpp
+++ Critter.hpp
@@ -1,32 +1,33 @@
-/********************************************************************* 
+/*********************************************************************
 ** Description: The specification file for the Critter base class
-*********************************************************************/ 
+*********************************************************************/
 
 #ifndef CRITTER_H
 #define CRITTER_H
 
-
 #include "Board.hpp"
+#include "constants.hpp"
+#include "logger.hpp"
 
 class Critter {
 
-protected:
-	int xLoc,yLoc;
-	int turnsSinceBreeding;
-	
+  protected:
+    int xLoc, yLoc;
+    int turnsSinceBreeding;
+    EntityTypes surroundings[4];
 
-public:
-	Critter();	
+  public:
+    Critter();
 
-	int getXLoc();
-	int getYLoc();
+    int getXLoc();
+    int getYLoc();
 
-	void setXLoc(int);
-	void setYLoc(int);
+    void setXLoc(int);
+    void setYLoc(int);
 
-
-	virtual void move(Board * board) = 0;
-	virtual bool canBreed() = 0;
+    void checkSurroundings(Board *board);
+    virtual void move(Board *board) = 0;
+    virtual bool canBreed() = 0;
 };
 
 #endif // CRITTER_H
