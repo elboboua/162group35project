2d75c603598c1f2ecbe1aa7ff3923a06aedcf51a
Eben <wighte@oregonstate.edu>
2018-11-01 22:52:22 -0700

    add storage for bugs

--- Game.hpp
+++ Game.hpp
@@ -1,30 +1,33 @@
-/********************************************************************* 
+/*********************************************************************
 ** Description: The specification for the Game class
-*********************************************************************/ 
+*********************************************************************/
 
 #ifndef GAME_H
 #define GAME_H
 
 #include "Ant.hpp"
 #include "Board.hpp"
+#include "Doodlebug.hpp"
 
 #include <iostream>
 
 class Game {
 
-private:
-	Ant * ant;
-	int antNum;
-	Board * board;
-	
-public: 
-	Game();
-	void loop();
-	void breedAnts();
+  private:
+    Ant *ant;
+    int antNum;
+    Doodlebug *bugs;
+    int bugNum;
+    Board *board;
 
-	// breedDoodlebug
-	// killDoodlebug
-	// killAnt
+  public:
+    Game();
+    void loop();
+    void breedAnts();
+
+    // breedDoodlebug
+    // killDoodlebug
+    // killAnt
 };
 
 #endif // GAME_H
